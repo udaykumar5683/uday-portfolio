c03525a370f6440b0266e02f17337c6252d36746
udaykumar5683 <167170824+udaykumar5683@users.noreply.github.com>
2025-08-21 15:33:49 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -11,11 +11,11 @@ const HeroPic = () => {
       viewport={{ once: false, amount: 0 }}
       className=" h-full flex items-center justify-center "
     >
-      <img
-        src="/images/HexaPic.png"
-        alt="uday kumar g"
-        className="max-h-[450px] w-auto"
-      />
+     <img
+      src="/images/HexaPic.png"
+      alt="uday kumar g"
+      className="h-[450px] w-[450px] object-cover"
+    />
 
       <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
